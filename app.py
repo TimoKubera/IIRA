1f6bad479854fdfe72970290af85cea5932a7aa7
shubh_v1 <verma.shubham9779@gmail.com>
2026-02-12 12:29:06 +0100

    docs: Update documentation
    - Modified 1 markdown file(s)
    - Created 0 new markdown file(s)
    - Added in-code docs to 1 source file(s)

--- app.py
+++ app.py
@@ -28,87 +28,68 @@ file_path = os.path.dirname(os.path.realpath(__file__))
 
 class App(tk.Tk):
     """
-    Die App-Klasse is für die Steuerung der Anwendung verantwortlich.
+    The App class is responsible for controlling the application.
 
-    Die App-Klasse erbt von der TK-Klasse und stellt bei der Initialisierung das
-    Root-Fenster zur Verfügung.
-    Zusätzlich übernimmt die Klasse die Steuerung der Anwendung, um weitere
-    GUI-Elemente anzeigen zu lassen und mit dem Nutzer zu interagieren.
+    The App class inherits from the TK class and provides the root window during initialization.
+    In addition, the class takes over the control of the application to display further GUI elements and interact with the user.
 
     Attributes:
-        title (str): Titel der Anwendung. Wird oben mittig im Fensterrahmen angezeigt.
-        geoometry (str): Größe des Fensters beim Aufrufen der App. Kann vom Nutzer
-                         angepasst werden.
-        rowconfigure (int, int): Das erste Integer-Argument gibt an welche Reihe im Grid-Layout-
-                                Manager ausgewählt werden soll. Das zweite Integer-Argument gibt ein
-                                Gewicht an. Gewiche größer als 0 signalisieren, dass die zusätzlich
-                                verfügbaren Platz ausfüllen soll, wenn die Fenstergröße verändert wird.
-                                Größere Gewiche werden dabei bevorzugt behandelt.
-        colconfigure (int, int): Analog zu rowconfigure, allerdings bei spalenweiser Betrachtung.
-        frames (dic): Enthält alle frames die in der Anwendung vorkommen können.
-                      Beispielsweise das MainFrame, oder das RateFrame, wo unterschiedliche
-                      Interaktionen ausgeführt werden können.
-    TODO icons in die Beschreibung; Styles einfügen
+        title (str): Title of the application. Displayed at the top center of the window frame.
+        geoometry (str): Size of the window when calling the app. Can be adjusted by the user.
+        rowconfigure (int, int): The first integer argument indicates which row should be selected in the grid layout manager. The second integer argument gives a weight. Weights greater than 0 signal that the additional available space should be filled when the window size is changed. Larger weights are preferred.
+        colconfigure (int, int): Analogous to rowconfigure, but with column-wise consideration.
+        frames (dic): Contains all frames that can occur in the application. For example, the MainFrame, or the RateFrame, where different interactions can be executed.
+    TODO icons in the description; Insert styles
     """
 
     def __init__(self):
         """
-        Konstruktor-Methode der App-Klasse.
+        Constructor method of the App class.
         """
         super().__init__()
-        # Alle Icons die in der Anwendung angezeigt werden.
         self.load_icons()
 
-        # Attribute die nicht mit tkinter zusammenhängen
-        #TODO Dummy-Werte löschen
         self.filevalidation = None
         self.dbinteraction = DBInteraction(os.path.join(file_path, "data/internal_db.csv"))
-        self.scale_format = ""      # Skalenformate sind nominal, ordinal, intervall und ratio.
+        self.scale_format = ""
         self.weights = ""
-        
-        #TODO löschen und auf Attribute von filevalidation zurückgreifen
         self.categories = []
         self.rater_ids = []
         self.text = []
         self.formatted_text = []
-        self.labels = {} # Label pro Text und Rater
+        self.labels = {}
 
-        # tkinter Attribute
         self.title("IIRA")
-        #self.iconphoto(False, self.app_icon)
         self.geometry("1500x750")
         self.minsize(1450, 750)
 
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-        self.light_mode = True  #TODO Wert aus DB auslesen, soblad darkmode implementiert wurde.
-        # ---> Inhalte die in den Frames angezeigt, oder gesetzt werden.
-        self.mode = None # Speichert, ob der User analysieren, oder bewerten will.
+        self.light_mode = True
+        self.mode = None
 
-        # Anwendungsweite Style-Konfigurationen
-        # Import the tcl file
         self.tk.call("source", os.path.join(file_path, "data/themes/forest-light.tcl"))
         self.style = ttk.Style()
-
-        # Set the theme with the theme_use method
-        #TODO dark mode kann implementiert werden. Dafür auch Icon-Farben verändern
         self.style.theme_use("forest-light")
 
-        self.frames = {} 
+        self.frames = {}
         self.init_frames()
         self.show_frame("MainFrame")
-    
+
     def show_frame(self, frame_name):
         """
-        Methode zum Wechseln des Frames der aktuell angezeigt wird.
+        Method to switch the frame that is currently displayed.
 
         Args:
-            frame_name (ttk.Frame): Der Frame, der angezeigt werden soll.
+            frame_name (ttk.Frame): The frame to be displayed.
         """
         frame = self.frames[frame_name]
         frame.tkraise()
-    
+
     def load_icons(self):
+        """
+        Load all icons displayed in the application.
+        """
         self.app_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/icons/intrarater_512px.png"))
         self.file_select_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/icons/file_select.png"))
         self.home_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/icons/home_32px.png"))
@@ -120,31 +101,29 @@ class App(tk.Tk):
         self.tooltip_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/icons/tooltip-16px.png"))
         self.save_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/icons/save_32px.png"))
         self.delete_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/icons/delete_32px.png"))
-        
-        #TODO Für darkmode anpassen, bzw löschen
         self.light_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/icons/light_mode_32px.png"))
         self.dark_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/icons/dark_mode.png"))
         self.unchecked_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/themes/forest-light/check-unsel-accent.png"))
         self.checked_icon = ImageTk.PhotoImage(file=os.path.join(file_path, "data/themes/forest-light/check-accent.png"))
 
     def init_root_frame(self, frame):
+        """
+        Initialize the root frame.
+
+        Args:
+            frame (ttk.Frame): The frame to be initialized.
+        """
         frame.grid(row=0, column=0, sticky="nsew")
-    
+
     def init_frames(self):
-        """ 
-        Initialisiert alle Frames, die es in der Software gibt. 
-        Die Frames werden in einem Dictionary gespeichert, um mit der show_frame-Funktion zwischen den Frames
-        wechseln zu können.
+        """
+        Initialize all frames in the software.
+        The frames are stored in a dictionary to be able to switch between the frames with the show_frame function.
         """
         for frame in self.frames:
-            # Löscht den Inhalt der frames.
-            # Ist beim startup der Applikation nicht von Bedeutung und wird dann übersprüngen, weil
-            # self.frames leer ist. Die Funktion wird aber auch benutzt, wenn der Home-Button angeklickt wird.
-            # In dem Fall werden die Fensterinhalte zurückgesetzt. 
             for widget in self.frames[frame].winfo_children():
                 widget.destroy()
 
-        # Frames initialisieren
         main_frame = MainFrame(self)
         self.init_root_frame(main_frame)
         self.frames["MainFrame"] = main_frame
